b78441a04b9c9340e030dec149eb6a5afba00497
yellowisher <yellowisher@naver.com>
2021-08-18 22:39:25 +0900

    commit

--- BOJ/2304_창고_다각형.py
+++ BOJ/2304_창고_다각형.py
@@ -5,7 +5,6 @@ N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 arr.sort()
 
-print(arr)
 max_num = 0
 for i in range(N):
     if max_num < arr[i][1]:
@@ -22,8 +21,7 @@ for i in range(mid):    # 0, 1, 2
     else:
         arr[i+1][1] = arr[i][1]
         cnt += (arr[i+1][0]-arr[i][0]) * arr[i][1]
-    print(cnt)
-print(arr)
+
 
 for i in range(N-1, mid, -1):     # 6, 5, 4
     if arr[i][1] <= arr[i-1][1]:
@@ -31,5 +29,5 @@ for i in range(N-1, mid, -1):     # 6, 5, 4
     else:
         arr[i-1][1] = arr[i][1]
         cnt += (arr[i][0]-arr[i-1][0]) * arr[i][1]
-    print(cnt)
-print(arr)
+
+print(cnt)
---
+++ BOJ/2559_수열.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, K = map(int, input().split())    # n = 100,000 -> O(n^2) 시간초과 주의
+arr = list(map(int, input().split()))
+
+result = 0
+for i in range(K):
+    result += arr[i]
+
+max_num = result
+for i in range(K, N):
+    result += (arr[i] - arr[i-K])   # 점화식 부분
+    if max_num < result:
+        max_num = result
+
+print(max_num)
