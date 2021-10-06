f4e9fc3555e6140c2b700a244ca9403995b43174
Lee-Geon-Yeong <59759468+Lee-Geon-Yeong@users.noreply.github.com>
2021-10-06 19:23:22 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,15 +1,53 @@
-import sys
-sys.stdin=open('input.txt')
+def merge_sort(Data):
+    if len(Data) <= 1:
+        return Data
 
-num = input()
+    mid = len(Data) // 2
+    left = merge_sort(Data[:mid])
+    right = merge_sort(Data[mid:])
+    return merge(left, right)
 
-for i in range(0, len(num)-7+1, 7):
-    bin = num[i:i+7] # ex) 000000, 1111000
+def merge(left, right):
+    global cnt
 
-    total = 0
+    left_N, right_N = len(left), len(right)
+    result = [0] * (left_N + right_N)
+    left_i, right_i = 0, 0
+    i = 0
 
-    for j in range(len(bin)-1, -1, -1): # 6,5,4,3,2,1,0
-        if bin[j] == '1':
-            total += 2 ** (len(bin) -1 -j) # 2**0, 2**1
+    if left[-1] > right[-1]:
+        cnt += 1
 
-    print(total, end=' ')
+    while left_i != left_N and right_i != right_N:
+        if left[left_i] <= right[right_i]:
+            result[i] += left[left_i]
+            i += 1
+            left_i += 1
+        else:
+            result[i] += right[right_i]
+            i += 1
+            right_i += 1
+
+    if left_i != left_N:
+        while left_i != left_N:
+            result[i] += left[left_i]
+            i += 1
+            left_i += 1
+
+    if right_i != right_N:
+        while right_i != right_N:
+            result[i] += right[right_i]
+            i += 1
+            right_i += 1
+
+    return result
+
+
+TC = int(input())
+for tc in range(1, TC+1):
+    N = int(input())
+    Data = list(map(int, input().split()))
+    cnt = 0
+    Data = merge_sort(Data)
+
+    print('#%d %d %d'%(tc, Data[N//2], cnt))
